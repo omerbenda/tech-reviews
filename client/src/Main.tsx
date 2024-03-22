fd4ad9d0b8ed228893033325747a65e12d42cdc9
omerbenda <34948058+omerbenda@users.noreply.github.com>
2024-03-22 14:28:20 +0200

    fix(Main.css, Main.tsx): Fix pages not having full screen height

--- client/src/Main.tsx
+++ client/src/Main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { CssBaseline } from '@mui/material';
 import { RouterProvider } from 'react-router-dom';
 import router from './Router.tsx';
+import './Main.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
