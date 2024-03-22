4d6aa5fdc63c41032b1b6912a1739a86f4d01deb
omerbenda <34948058+omerbenda@users.noreply.github.com>
2024-03-22 14:49:05 +0200

    feat(Navbar.tsx): Add navigating back to home screen on navbar title link

--- client/src/Common/Components/Navbar/Navbar.tsx
+++ client/src/Common/Components/Navbar/Navbar.tsx
@@ -1,6 +1,13 @@
 import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleTitleClick = () => {
+    navigate('/');
+  };
+
   return (
     <AppBar position="static">
       <Container maxWidth="xl" sx={{ m: 0 }}>
@@ -9,6 +16,7 @@ const Navbar = () => {
             variant="h6"
             color="inherit"
             noWrap
+            onClick={handleTitleClick}
             sx={{ cursor: 'pointer', userSelect: 'none' }}
           >
             Tech Reviews
